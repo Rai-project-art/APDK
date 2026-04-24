ff8f4551675860c2a701ed25105eed90a97764d5
Rai-project-art <raitz4237@gmail.com>
2026-04-24 13:15:25 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAw7vzgjvLQfCklMKvdQYFQb7MKpNn8lL4",
+  authDomain: "pusat-data-96c4b.firebaseapp.com",
+  projectId: "pusat-data-96c4b",
+  storageBucket: "pusat-data-96c4b.firebasestorage.app",
+  messagingSenderId: "825366165936",
+  appId: "1:825366165936:web:e8a91875aa8d4a43c33e75"
+};
+
